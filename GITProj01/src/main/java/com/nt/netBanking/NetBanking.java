2534437477142ed3e4eb4953fa0e82ecf5d26180
PRASAD JADHAV <107417989+prasadjadhav12@users.noreply.github.com>
2024-11-12 15:39:03 +0530

    dev1 opening logic

--- GITProj01/src/main/java/com/nt/netBanking/NetBanking.java
+++ GITProj01/src/main/java/com/nt/netBanking/NetBanking.java
@@ -1,7 +1,13 @@
 package com.nt.netBanking;
 
+import java.util.Random;
+
 public class NetBanking {
-	
+	public String OpenAccount(String aadhar,double initailAmount,String type)
+
+	{
+		return "acount is open for "+new Random().nextLong(1234567890);
+	}
 	
 
 }
