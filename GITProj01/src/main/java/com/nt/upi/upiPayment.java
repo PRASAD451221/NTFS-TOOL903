fb3d6ff9fdb6189020a379395d6faed4acbe647e
PRASAD JADHAV <107417989+prasadjadhav12@users.noreply.github.com>
2024-11-12 14:16:36 +0530

    tl chage the code brach 1 code

--- GITProj01/src/main/java/com/nt/upi/upiPayment.java
+++ GITProj01/src/main/java/com/nt/upi/upiPayment.java
@@ -1,9 +1,15 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class upiPayment {
-	public String upipay(String upiid,Double amout)
+	public String upipay(String upiid,float amout)
 	{
 		return "upi payment for the amount" + amout+"this id"+upiid+" as done";
+	}
+	public double checkBalance(String upid)
+	{
+		return new Random().nextInt(122000);
 	}
 
 }
