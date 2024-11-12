1b34d6db4dbb472caf0b79a9896f921b322facf4
PRASAD JADHAV <107417989+prasadjadhav12@users.noreply.github.com>
2024-11-12 14:05:18 +0530

    dev 1 upi payment  deplopment

---
+++ GITProj01/src/main/java/com/nt/upi/upiPayment.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class upiPayment {
+	public String upipay(String upiid,Double amout)
+	{
+		return "upi payment for the amount" + amout+"this id"+upiid+" as done";
+	}
+
+}
